b518469ddc8a52d59219bda529c9bab7572bc81a
NGÔ THANH THIÊN <thanhthien.devp@gmail.com>
2024-12-04 16:31:24 +0700

    prod feat: main page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+      serveStaticOptions: {
+        cacheControl: true,
+        maxAge: 43200000,
+        immutable: true,
+      },
+    }),
+  ],
 })
 export class AppModule {}
