4e831fe492ed8bcd80ea4ca7d8d4a6c7f2abfaac
NGÔ THANH THIÊN <thanhthien.devp@gmail.com>
2025-04-29 16:21:53 +0700

    dev: fix issue displaying api docs

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
   const port = process.env.PORT || 3000;
@@ -16,9 +14,6 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET, POST, PUT, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization',
-  });
-  app.useStaticAssets(path.join(__dirname, 'swagger-ui'), {
-    prefix: '/api/swagger/', // URL route for Swagger UI
   });
   const config = new DocumentBuilder()
     .setTitle('OPSHEE API')
@@ -35,8 +30,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
-
+  SwaggerModule.setup('api/swagger', app, document, {
+    customCssUrl:
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css',
+    customJs: [
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js',
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-standalone-preset.js',
+    ],
+  });
   await app.listen(port, () => {
     console.log(`server is now running on port: ${port}`);
   });
